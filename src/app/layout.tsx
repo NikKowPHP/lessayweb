526ed72ec91d07645865ea8293e78aa264cc5c0e
NikKowPHP <nik.kow@outlook.com>
2025-01-04 08:36:52 +0100

    refactor: Use use client in client components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,28 @@
-import { Inter } from "next/font/google";
-import type { Metadata } from "next";
+import { Inter } from 'next/font/google'
+import type { Metadata } from 'next'
 import ReduxProvider from '@/store/provider'
-import Layout from "@/components/layout/Layout";
-import "@/styles/globals.css";
+import Layout from '@/components/layout/Layout'
+import '@/styles/globals.css'
 
-const inter = Inter({ 
-  subsets: ["latin"],
+const inter = Inter({
+  subsets: ['latin'],
   display: 'swap',
   variable: '--font-inter',
-});
+})
 
 export const metadata: Metadata = {
   title: {
-    default: "lessay",
-    template: "%s | lessay"
+    default: 'lessay',
+    template: '%s | lessay',
   },
-  description: "Personalized language learning web app using AI to tailor the learning path based on an initial skills assessment.",
-  viewport: "width=device-width, initial-scale=1",
-};
+  description:
+    'Personalized language learning web app using AI to tailor the learning path based on an initial skills assessment.',
+}
+
+export const viewport = {
+  width: 'device-width',
+  initialScale: 1,
+}
 
 export default function RootLayout({
   children,
@@ -28,11 +33,9 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body className={`${inter.variable} font-sans antialiased`}>
         <ReduxProvider>
-          <Layout>
-            {children}
-          </Layout>
+          <Layout>{children}</Layout>
         </ReduxProvider>
       </body>
     </html>
-  );
-} 
+  )
+}

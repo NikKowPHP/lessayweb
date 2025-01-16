ca15c6cd3f9b3ee5c6be2c024183d6a0f1e4ab10
NikKowPHP <nik.kow@outlook.com>
2025-01-16 10:20:00 +0100

    feat: Register exercise slice

--- src/store/index.ts
+++ src/store/index.ts
@@ -4,13 +4,16 @@ import userReducer from './slices/userSlice'
 import learningReducer from './slices/learningSlice'
 import onboardingReducer from './slices/onboardingSlice'
 import recordingReducer from './slices/recordingSlice'
+import exercisingReducer from './slices/exercisingSlice'
+
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     user: userReducer,
     learning: learningReducer,
     onboarding: onboardingReducer,
-    recording: recordingReducer
+    recording: recordingReducer,
+    exercising: exercisingReducer
   },
 })
 
